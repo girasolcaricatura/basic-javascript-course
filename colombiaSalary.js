befa00ab3564d3c2a7cd98f82926e92148720c82
Emiliano Diaz <girasolcaricatura@gmail.com>
2021-11-22 11:54:55 -0600

    working on the select your country section on pay gap page

--- colombiaSalary.js
+++ colombiaSalary.js
@@ -100,3 +100,107 @@ colombia.push({
   salary: 100000000,
   gender: "Male",
 });
+
+
+const mexico = [];
+
+mexico.push({
+  name: "Camila",
+  salary: 50,
+  gender: "Female",
+});
+mexico.push({
+  name: "Jose",
+  salary: 1500,
+  gender: "Male",
+});
+mexico.push({
+  name: "Luisa",
+  salary: 100,
+  gender: "Female",
+});
+mexico.push({
+  name: "Pedro",
+  salary: 1000,
+  gender: "Male",
+});
+mexico.push({
+  name: "Daniela",
+  salary: 2200,
+  gender: "Female",
+});
+mexico.push({
+  name: "Emiliano",
+  salary: 200,
+  gender: "Male",
+});
+mexico.push({
+  name: "Carla",
+  salary: 50,
+  gender: "Female",
+});
+mexico.push({
+  name: "Luis",
+  salary: 1500,
+  gender: "Male",
+});
+mexico.push({
+  name: "Alicia",
+  salary: 1000,
+  gender: "Female",
+});
+mexico.push({
+  name: "Pedro",
+  salary: 2900,
+  gender: "Male",
+});
+mexico.push({
+  name: "Julia",
+  salary: 300,
+  gender: "Female",
+});
+mexico.push({
+  name: "Roberto",
+  salary: 400,
+  gender: "Male",
+});
+mexico.push({
+  name: "Angélica",
+  salary: 400,
+  gender: "Female",
+});
+mexico.push({
+  name: "Victor",
+  salary: 4000,
+  gender: "Male",
+});
+mexico.push({
+  name: "Lorena",
+  salary: 600,
+  gender: "Female",
+});
+mexico.push({
+  name: "Carolina",
+  salary: 600,
+  gender: "Female",
+});
+mexico.push({
+  name: "Ricardo",
+  salary: 2600,
+  gender: "Male",
+});
+mexico.push({
+  name: "Nora",
+  salary: 900,
+  gender: "Female",
+});
+mexico.push({
+  name: "Gisselle",
+  salary: 200,
+  gender: "Female",
+});
+mexico.push({
+  name: "Billy Joel",
+  salary: 100000,
+  gender: "Male",
+});
